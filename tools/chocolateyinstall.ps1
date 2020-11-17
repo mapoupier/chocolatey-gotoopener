6c431da60785af562e691e122640f57ede2b578f
Christoph "SuperVirus" Heitkamp <dev@chrisheitkamp.de>
2020-11-17 16:31:02 +0100

    Added build recipe

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://launch.getgo.com/builds/launcher/536/GoToOpenerMultiUser.msi'
+$checksum   = '7dcbb4d57bcef657f41332cffda01d0307f0336a56291a6f8416e6498999dbaa'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+
+  softwareName  = 'GoTo Opener (Multi User)'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
